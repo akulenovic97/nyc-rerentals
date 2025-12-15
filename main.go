afc54531cd33241933dcfb5e27e1b8e05f0e1623
akulenovic97 <akulenovic97@gmail.com>
2025-12-14 22:43:41 -0500

    feat: add reside ny fetch

--- main.go
+++ main.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
+	"log"
+	"github.com/gocolly/colly/v2"
 )
 
 func main() {
 	fmt.Println("NYC re-rentals scraper")
 
-	resp, err := http.Get("https://residenewyork.com/my-properties/?status=open-to-applications")
-	if err != nil {
-		fmt.Println("Error with fetch: ", err)
-		return
-	}
-	defer resp.Body.Close()
+	c := colly.NewCollector()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error with read: ", err)
-		return
-	}
-	fmt.Printf("Got %d bytes", len(body))
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Now visiting ", r.URL)
+	})
+	c.OnHTML(".rh_page__listing article h3 a", func(e *colly.HTMLElement) {
+		name := e.Text
+		link := e.Attr("href")
+		fmt.Println("Property: ", name)
+		fmt.Println("Link: ", link)
+	})
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Request failed: ", err)
+	})
+	c.Visit("https://residenewyork.com/my-properties/?status=open-to-applications")
 }
